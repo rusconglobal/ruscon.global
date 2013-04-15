58eb7453914e2b2271688a4ae479fbfbcfd66915
Pavel <picasso@picasso-kubuntu-vb.(none)>
2013-04-15 14:57:13 +0400

    fix

--- geo_office/admin.py
+++ geo_office/admin.py
@@ -3,24 +3,29 @@ from geo_office.models import OfficeContact, Office, Country, Town, OfficeType,\
     ContactType
 from modeltranslation.admin import TranslationAdmin
 
-class ContactInline(admin.TabularInline):
-    model = OfficeContact
-
-class OfficeAdmin(TranslationAdmin):    
-    list_display = ('town',) 
-    inlines = [ContactInline,]
+class ModelTranslationMedia(object):    
     class Media:
         js = (
             'modeltranslation/js/force_jquery.js',
             'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
+            'modeltranslation/js/tabbed_translation_fields.js',           
         )
         css = {
             'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
+        } 
+
+class ContactInline(admin.TabularInline):
+    model = OfficeContact
+
+class OfficeAdmin(TranslationAdmin, ModelTranslationMedia):    
+    list_display = ('town',) 
+    inlines = [ContactInline,]    
+
+class TransAdmin(TranslationAdmin, ModelTranslationMedia):
+    pass
 
 admin.site.register(Office,OfficeAdmin)
-admin.site.register(Country)
-admin.site.register(Town)
-admin.site.register(OfficeType)
-admin.site.register(ContactType)
+admin.site.register(Country,TransAdmin)
+admin.site.register(Town,TransAdmin)
+admin.site.register(OfficeType,TransAdmin)
+admin.site.register(ContactType,TransAdmin)
