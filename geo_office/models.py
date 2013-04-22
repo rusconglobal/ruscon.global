c79ec2393dc396c6cc3be161b09facde65fd9995
Pavel <picasso@picasso-kubuntu-vb.(none)>
2013-04-22 10:44:10 +0400

    fix

--- geo_office/models.py
+++ geo_office/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from filer.fields.image import FilerImageField
 from orderedmodel.models import OrderedModel
 from cms.models.pluginmodel import CMSPlugin
