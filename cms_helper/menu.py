adcbb93474a7204278c09e6a978ea6ef47fb2937
314casso <picasso75@yandex.ru>
2016-12-08 12:08:00 +0300

    fix

--- cms_helper/menu.py
+++ cms_helper/menu.py
@@ -1,7 +1,6 @@
 from cms.menu_bases import CMSAttachMenu
-from menus.base import Menu, NavigationNode
-from menus.menu_pool import menu_pool
-from django.core.urlresolvers import reverse
+from menus.base import NavigationNode
+
 from django.utils.translation import ugettext_lazy as _
 
 class BlogMenu(CMSAttachMenu):
