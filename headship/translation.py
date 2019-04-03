a27bdd93886cb607f3450ca15ce55fafdaa66f9c
314casso <picasso75@yandex.ru>
2019-04-03 14:59:29 +0300

    fix

--- headship/translation.py
+++ headship/translation.py
@@ -2,6 +2,6 @@ from modeltranslation.translator import translator, TranslationOptions
 from headship.models import Headship
 
 class HeadshipTranslationOptions(TranslationOptions):
-    fields = ('first_name','last_name','patronymic','post')
+    fields = ('first_name','last_name','patronymic','post','subpost','biopic')
     
 translator.register(Headship, HeadshipTranslationOptions)    
