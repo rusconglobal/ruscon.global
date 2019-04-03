f0cca5006d8f824024d0eb22933d47b3ccf30a1e
314casso <picasso75@yandex.ru>
2019-04-03 14:51:40 +0300

    fix

--- headship/models.py
+++ headship/models.py
@@ -15,8 +15,17 @@ class Headship(models.Model):
     phone = models.CharField(_('Phone'), max_length=100, blank=True, null=True)
     image = FilerImageField(blank=True, null=True)
     active = models.BooleanField(_('Active'), default=False)
-    sort_order = models.IntegerField(_('Sort'), default=0) 
+    sort_order = models.IntegerField(_('Sort'), default=0)
+    subpost = models.CharField(_('Sub position'), max_length=255, blank=True, null=True)
+    biopic = models.TextField(_('Biopic'), blank=True, null=True)
+    show_biopic = models.BooleanField(_('Show biopic'), default=False)
+    
+    def extraclass(self):
+        if self.show_biopic:
+            return 'custom-modal-trigger'
+    
     def __unicode__(self):
         return u'%s %s %s' % (self.first_name, self.patronymic ,self.last_name)
+    
     class Meta:
         ordering = ['sort_order']
