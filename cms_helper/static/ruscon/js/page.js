9bd0c5aea7879d116072998298e4468ac6ed96e1
314casso <picasso75@yandex.ru>
2020-05-28 10:42:33 +0300

    fix

---
+++ cms_helper/static/ruscon/js/page.js
@@ -0,0 +1,15 @@
+$('.collapse2').click(function() {
+	collapse2(this, 800);
+})
+
+$('.collapse2').each(function() {
+	collapse2(this, 0);
+})
+
+function collapse2(elem, delay) {
+	$(elem).parent().next().each(function() {
+		$(this).slideToggle(800);
+		$(this).next().slideToggle(800);
+	})
+} 
+	
