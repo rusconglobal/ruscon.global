3f80d1a1604ebd075708fbde35d6ac68e8dde427
Picasso <it-support2@ruscon.global>
2021-07-19 01:29:07 +0300

    fix

--- rusconwww/settings.py
+++ rusconwww/settings.py
@@ -26,7 +26,7 @@ MANAGERS = ADMINS
 
 DEFAULT_LANGUAGE = 'ru'
 
-LANGUAGES = [('ru', u'Русский'), ('en', u'English'), ('zh-cn', u'简体中文')]
+LANGUAGES = [('ru', u'Русский'), ('en', u'English')]
 
 from local_settings import * #@UnusedWildImport
 
