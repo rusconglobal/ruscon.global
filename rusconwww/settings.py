30a7d35a6d568220cb0453b4e2ff59b5c5ef732c
Picasso <it-support2@ruscon.global>
2021-07-19 00:50:30 +0300

    fix

--- rusconwww/settings.py
+++ rusconwww/settings.py
@@ -156,7 +156,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.staticfiles',
-    #'haystack',       
+    'haystack',       
     'cms',
     'menus',
     'mptt',
